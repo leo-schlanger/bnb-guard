dedfc8c959d237d71612bcde2e9656459ad79724
leo-schlanger <leoschlanger@gmail.com>
2025-05-17 00:24:27 -0300

    change key and using env

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-BSCSCAN_API_KEY = "X1V8NSAMRZC7XUJ21HAHS14SDF7YCBEAXF"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BSCSCAN_API_KEY = os.getenv("BSCSCAN_API_KEY")
+
+if not BSCSCAN_API_KEY:
+    raise RuntimeError("❌ BSCSCAN_API_KEY não definida no .env")
